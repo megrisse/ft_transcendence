a255776f88ac9d80c59a080c1d7ec00679152e51
Merouane Grissen <mgrissen9@gmail.com>
2023-12-28 21:41:54 +0100

    Finish product

--- Back/script.sh
+++ Back/script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# npx prisma generate || exit 1
-npx prisma db push || exit 1
-npm run build || exit 1
+npx prisma db push
+npm run build
 npm run start
-
-
-# #!/bin/bash
-
-# npm run prisma:generate
-
-# npx prisma db push
-
-# npm run build
-
-# npm run start
